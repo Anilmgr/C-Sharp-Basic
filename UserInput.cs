e43446863a619897fb52ec8aced8b0468bdfe1ed
Anil Baral <mgranil60@gmail.com>
2020-12-09 17:16:47 +0545

    User Input in Command in C#.

---
+++ UserInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBasics
+{
+    class UserInput
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine(); 
+            Console.WriteLine("Hello " + name +"!");
+
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine()); //type conversion since ReadLine reads String
+            Console.WriteLine("Age " + age);
+        }
+    }
+}
