95f38c9924b3c771f868271bada2d887cf48be39
Anil Baral <mgranil60@gmail.com>
2020-12-09 17:18:08 +0545

    Use of Math Class in C#.

---
+++ MathClassMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBasics
+{
+    class MathClassMethods
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Max(5,10));   //returns 10
+            Console.WriteLine(Math.Min(5,10));   //returns 5
+            Console.WriteLine(Math.Abs(-4.7));   //returns 4.7
+            Console.WriteLine(Math.Sqrt(25));   //returns 5
+            Console.WriteLine(Math.Round(4.9));   //returns 5
+            Console.WriteLine(Math.Round(4.4));   //returns 4
+
+        }
+    }
+}
