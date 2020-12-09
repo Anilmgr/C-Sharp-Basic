e72c0d86c35105aa1621497099b20d26fd896b6f
Anil Baral <mgranil60@gmail.com>
2020-12-09 16:22:15 +0545

    Hello to C#.

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpBasics
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
