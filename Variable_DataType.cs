a6025f46cfbdc7057db945002dab857ccef974ac
Anil Baral <mgranil60@gmail.com>
2020-12-09 17:16:21 +0545

    Variables and Data Types in C#.

---
+++ Variable_DataType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBasics
+{
+    class Variable_DataTye
+    {
+        static void Main(String[] args)
+        {
+            //datatype variable = value
+
+            string name = "Apple";
+            Console.WriteLine("String: "+name);
+
+            int num1 = 10;
+            Console.WriteLine("Integer: "+num1);
+
+            char letter = 'a';
+            Console.WriteLine("Character: "+letter);
+
+            double num2 = 1.23;
+            Console.WriteLine("Double: "+num2);
+
+            bool flag = false;
+            Console.WriteLine("Boolean: "+flag);
+
+
+        }
+    }
+}
