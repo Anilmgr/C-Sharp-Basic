6b7eb9d283c84f570da7f8279c3db324b3e83632
Anil Baral <mgranil60@gmail.com>
2020-12-09 17:50:57 +0545

    Boolean Operations in C#.

---
+++ BooleanOp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharpBasics
+{
+    class BooleanOp
+    {
+        static void Main( string[] args)
+        {
+            bool enjoyingCSharp = true;
+            bool feelingLazy = false;
+            Console.WriteLine(enjoyingCSharp);   // Outputs True
+            Console.WriteLine(feelingLazy);   // Outputs False
+
+            // Comparison operator returns boolean value
+
+            Console.WriteLine(5 < 10); // Outputs True
+            Console.WriteLine(5 == 10); // Outputs Falses
+        }
+    }
+}
